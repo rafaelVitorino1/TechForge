a36ce06b26be65f2bb4ce254126c340cc76e9a9f
rafael.vitorino <rafael.alves0028@gmail.com>
2024-04-19 20:14:48 -0300

    coments

--- resources/views/postagem/show.blade.php
+++ resources/views/postagem/show.blade.php
@@ -19,10 +19,36 @@
                     <strong>Atualização:</strong>   {{ \Carbon\Carbon::parse($postagem->updated_at)->format('d/m/Y h:i:s') }}<br>
                 </div>
 
+
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Comentarios</div>
+
+                    <div class="card-body">
+                        @foreach ( $postagem->comentarios as $value )
+
+                            <strong>{{ $postagem->user->name }}:</strong><br>
+                            <p>{{ $value->conteudo }}</p>
+                        
+                        @endforeach
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
+
+
 
 
